d84026696089d09128efcf95d52c65d6becb1823
Matthew McGuff <matthewmcguff85@gmail.com>
2018-04-04 08:25:50 -0600

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=elderbot-Web-Deploy.pubxml -p:Password=g2HJoQRtpsWGdJE7addlupQNGhvyoAHtqutxQ3q5lPiSpyG8cZoflSX2x2oj
+
